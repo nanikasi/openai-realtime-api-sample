e6c9896f44a62e94e32faa5a3b7855c494864c2d
nanikasi <nanikasi1024@gmail.com>
2024-10-11 23:04:10 +0900

    add query params

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ import fastifyFormBody from "@fastify/formbody";
 import fastifyWs from "@fastify/websocket";
 import dotenv from "dotenv";
 import Fastify from "fastify";
-import WebSocket from "ws";
+import * as fs from "node:fs";
 import twilio from "twilio";
-import fs from "fs";
+import WebSocket from "ws";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Retrieve the OpenAI API key from environment variables. You must have OpenAI Realtime API access.
-const { OPENAI_API_KEY, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER } = process.env;
+const {
+  OPENAI_API_KEY,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER,
+} = process.env;
 
-if (!OPENAI_API_KEY || !TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_PHONE_NUMBER) {
-    console.error("Missing required environment variables. Please set them in the .env file.");
-    process.exit(1);
+if (
+  !OPENAI_API_KEY ||
+  !TWILIO_ACCOUNT_SID ||
+  !TWILIO_AUTH_TOKEN ||
+  !TWILIO_PHONE_NUMBER
+) {
+  console.error(
+    "Missing required environment variables. Please set them in the .env file.",
+  );
+  process.exit(1);
 }
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
@@ -25,10 +37,10 @@ fastify.register(fastifyFormBody);
 fastify.register(fastifyWs);
 
 // Constants
-const SYSTEM_MESSAGE = fs.readFileSync("./prompt.v1.txt", 'utf8');
+const SYSTEM_MESSAGE = fs.readFileSync("./prompt.txt", "utf8");
 const VOICE = "alloy";
 const PORT = process.env.PORT || 5050; // Allow dynamic port assignment
-const HOST = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
+const HOST = "RENDER" in process.env ? "0.0.0.0" : "localhost";
 
 // List of Event Types to log to the console. See OpenAI Realtime API Documentation. (session.updated is handled separately.)
 const LOG_EVENT_TYPES = [
@@ -50,46 +62,49 @@ fastify.get("/", async (request, reply) => {
   reply.send({ message: "Twilio Media Stream Server is running!" });
 });
 
+fastify.post("/call", async (request, reply) => {
+  const { number, restaurant_number, name, reserve_date } = request.query;
+
+  if (!number || !restaurant_number || !name || !reserve_date) {
+    return reply.status(400).send({ error: "vars are required" });
+  }
+
+  try {
+    // Make the call using Twilio
+    const call = await client.calls.create({
+      to: restaurant_number, // The number to call
+      from: TWILIO_PHONE_NUMBER, // Your Twilio phone number
+      url: `https://${request.headers.host}/incoming-call?number=${encodeURIComponent(number)}&name=${encodeURIComponent(name)}&reserveDate=${encodeURI(reserve_date)}`, // The URL for TwiML instructions
+    });
+
+    reply.send({ message: `Calling ${number}`, callSid: call.sid });
+  } catch (error) {
+    console.error("Error making call:", error);
+    reply.status(500).send({ error: "Failed to make the call" });
+  }
+});
+
 // Route for Twilio to handle incoming and outgoing calls
 // <Say> punctuation to improve text-to-speech translation
 fastify.all("/incoming-call", async (request, reply) => {
+  const { number, name, reserve_date } = request.query;
   const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
-                              <Say>O.K. you can start talking!</Say>
+                              <Say>O.K.</Say>
                               <Connect>
-                                  <Stream url="wss://${request.headers.host}/media-stream" />
+                                  <Stream url="wss://${request.headers.host}/media-stream?number=${encodeURIComponent(number)}&name=${encodeURIComponent(name)}&reserveDate=${encodeURI(reserve_date)}" />
                               </Connect>
                           </Response>`;
 
   reply.type("text/xml").send(twimlResponse);
 });
 
-fastify.post("/call", async (request, reply) => {
-    const number = "+818083241269";
-  
-    if (!number) {
-      return reply.status(400).send({ error: "Phone number is required" });
-    }
-  
-    try {
-      // Make the call using Twilio
-      const call = await client.calls.create({
-        to: number, // The number to call
-        from: TWILIO_PHONE_NUMBER, // Your Twilio phone number
-        url: `https://${request.headers.host}/incoming-call` // The URL for TwiML instructions
-      });
-  
-      reply.send({ message: `Calling ${number}`, callSid: call.sid });
-    } catch (error) {
-      console.error("Error making call:", error);
-      reply.status(500).send({ error: "Failed to make the call" });
-    }
-});
-
 // WebSocket route for media-stream
 fastify.register(async (fastify) => {
   fastify.get("/media-stream", { websocket: true }, (connection, req) => {
     console.log("Client connected");
+
+    const { number, name, reserve_date } = req.query;
 
     const openAiWs = new WebSocket(
       "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01",
@@ -104,6 +119,8 @@ fastify.register(async (fastify) => {
     let streamSid = null;
 
     const sendSessionUpdate = () => {
+      const prompt = `${SYSTEM_MESSAGE}\n- 【予約時間】${reserve_date}\n- 【電話番号】${number} \n- 【名前】${name}`;
+
       const sessionUpdate = {
         type: "session.update",
         session: {
@@ -111,10 +128,7 @@ fastify.register(async (fastify) => {
           input_audio_format: "g711_ulaw",
           output_audio_format: "g711_ulaw",
           voice: VOICE,
-          instructions: SYSTEM_MESSAGE + `
-- 【予約時間】2024/10/12/18:00:00
-- 【電話番号】080 1234 5678
-- 【名前】なかにし　なおと`,
+          instructions: prompt,
           modalities: ["text", "audio"],
           temperature: 0.8,
         },
